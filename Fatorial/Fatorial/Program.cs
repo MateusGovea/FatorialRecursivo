a52b8f57dcfe722023d53ea3bfefd039b3b095e4
MateusGovea <90796629+MateusGovea@users.noreply.github.com>
2022-06-02 09:47:26 -0300

    Add files via upload

---
+++ Fatorial/Fatorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número para calcular o fatorial: ");
+            int num = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("O fatorial de " + num + "é: " + Fatorial(num));
+
+            Console.ReadKey();
+        }
+
+        static int Fatorial(int num)
+        {
+            if (num <= 0)
+            {
+                return 1;
+            }
+            return num * Fatorial(num - 1);
+        }
+    }
+}
